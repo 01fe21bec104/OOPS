1935f98514632a4c60d7b064b79647e737cc2265
01fe21bec104-N <105065125+01fe21bec104@users.noreply.github.com>
2024-03-12 22:58:20 +0530

    Create Temperature_convertions.cpp

---
+++ Temperature_convertions.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+   float t,f,c;
+   cout<<"enter temperature";
+   cin>>t;
+   f=(t*9/5)+32;
+   cout<<"Temperature in Fahrenheit "<<f<<endl;
+   c=(t-32)*5/9;
+   cout<<"Temperature in celcius "<<c<<endl;
+
+
+}
+

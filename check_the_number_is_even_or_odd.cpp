03dde9d89000b8cc6cd66b9effc37b6325902c86
01fe21bec104-N <105065125+01fe21bec104@users.noreply.github.com>
2024-03-12 21:49:47 +0530

    Update and rename basic.cpp to check_the_number_is_even_or_odd.cpp

---
+++ check_the_number_is_even_or_odd.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"enter the number";
+    cin>>num;
+    if(num%2==0)
+    {
+        cout<<"number is even";
+    }
+    else
+    {
+        cout<<"number is odd";
+    }
+}

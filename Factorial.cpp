abf98eb594deffdd7541d132a3a4e8488040ec24
01fe21bec104-N <105065125+01fe21bec104@users.noreply.github.com>
2024-03-12 23:12:06 +0530

    Create Factorial.cpp

---
+++ Factorial.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int n,fact=1;
+    cout<<"enter n";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        fact=fact*i;
+    }
+    cout<<fact;
+}

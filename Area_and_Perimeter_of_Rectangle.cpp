fc89b08b4a8833559335376c0003bb66993eedd1
01fe21bec104-N <105065125+01fe21bec104@users.noreply.github.com>
2024-03-12 21:53:59 +0530

    Create Area_and_Perimeter_of_Rectangle.cpp

---
+++ Area_and_Perimeter_of_Rectangle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int l,b;
+    cout<<"enter l and b";
+    cin>>l>>b;
+    cout<<"area :"<<l*b<<endl;
+    cout<<"perimeter :"<<2*(l+b)<<endl;
+}

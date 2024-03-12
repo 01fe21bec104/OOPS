14c2b46d7376af4e6c1b5b9bbd24b4ec92ba6afc
01fe21bec104-N <105065125+01fe21bec104@users.noreply.github.com>
2024-03-12 20:26:16 +0530

    Create basic.cpp

---
+++ basic.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name,country,state;
+    int sem;
+    cout<<"name,country,state,sem";
+    cin>>name>>country>>state>>sem;
+    cout<<"name:"<<name<<"\t country:"<<country<<"\t state:"<<state<<"\t sem: "<<sem;
+}

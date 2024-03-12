34a4b3eb2af30f8472bf2172be0186020114566e
01fe21bec104-N <105065125+01fe21bec104@users.noreply.github.com>
2024-03-12 21:45:47 +0530

    Create Swapping_of_two_Numbers.cpp

---
+++ Swapping_of_two_Numbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int n1,n2,temp;
+    cout<<"enter n1 and n2";
+    cin>>n1>>n2;
+    temp=n1;
+    n1=n2;
+    n2=temp;
+    cout<< n1<<endl;
+     cout<< n2<<endl;
+
+}
